9dc897d0b2e6a09a2b338c30181f5af78e23e227
Ronit Bhujel <ronitbhujel76@gmail.com>
2023-08-12 10:28:03 +0545

    changes

--- question 1/MinimumCostOfClothing.java
+++ question 1/MinimumCostOfClothing.java
@@ -1,5 +1,7 @@
 public class MinimumCostOfClothing {
+
     public static int findMinimumCost(int[][] price) {
+
         int N = price.length;
         if (N != 3) {
             throw new IllegalArgumentException("The number of people (N) must be 3.");
@@ -28,7 +30,9 @@ public class MinimumCostOfClothing {
             {11, 14, 3},
             {14, 2, 10}
         };
+
         int result = findMinimumCost(price);
         System.out.println("Minimum cost required: " + result); // Output: 9
     }
+    
 }
