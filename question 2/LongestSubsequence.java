0bb0ade1180c5da153af9f9ceee365344bde32a3
Ronit Bhujel <ronitbhujel76@gmail.com>
2023-08-12 10:31:45 +0545

    changes

--- question 2/LongestSubsequence.java
+++ question 2/LongestSubsequence.java
@@ -1,11 +1,15 @@
 public class LongestSubsequence {
+
     public static int longestSubsequence(int[] nums, int k) {
+
         int n = nums.length;
         int[] dp = new int[n];
         dp[0] = 1;
 
         for (int i = 1; i < n; i++) {
-            dp[i] = 1;
+            dp[i] = 1;      // initialize each element as a subsequence of length 1 
+
+            // find the maximum length subsequence ending at idnex 1 
             for (int j = 0; j < i; j++) {
                 if (nums[i] < nums[j] && Math.abs(nums[i] - nums[j]) <= k) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
@@ -13,6 +17,7 @@ public class LongestSubsequence {
             }
         }
 
+        // find the maximum value in the dp array 
         int maxLength = 0;
         for (int length : dp) {
             maxLength = Math.max(maxLength, length);
@@ -27,4 +32,5 @@ public class LongestSubsequence {
         int result = longestSubsequence(nums, k);
         System.out.println("Length of the longest subsequence: " + result); // Output: 5
     }
+    
 }
